b5e697e8485de8b2e5c0533ca9e106af628030de
Stephanie Wang <swang@cs.berkeley.edu>
2022-05-10 15:27:30 -0700

    notes

--- gpu_buffer_option3/mock.py
+++ gpu_buffer_option3/mock.py
@@ -5,6 +5,20 @@ import uuid
 from ray import ObjectRef
 
 
+# Locality for GpuObjects?
+# 1. Datasets only - we can extend Datasets actor pool strategy to take in locality hints from previous stages.
+# 2. more general Ray core solution - given an ObjectRef, we can tell that it contains a GpuObjectRef, and we can get the actor handle that created the GPU buffer.
+#    a. We use ray.get to figure out that it's a GpuObjectRef and then extract a handle to the actor.
+#    b. Given any ObjectRef, Ray is aware that it's actually a GpuObjectRef without having to call ray.get.
+
+
+# Options for GPU buffer passing:
+# 1. Transfer through plasma.
+# 2. Keep the GPU buffer local to the actor - requires scheduling dependent task on the same actor that created the buffer.
+# 3. Keep the buffer on the same GPU, but send to a different actor via IPC.
+# 4. Send to a different actor via NCCL/collectives.
+
+
 class GpuObjectRef:
     """Presents a reference to GPU buffer."""
 
